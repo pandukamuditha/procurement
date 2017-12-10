37ff67e3e682f3bb5a7c8c22447a9993a5a4c2ad
Panduka Muditha <pandukamudhitha@gmail.com>
2017-12-10 08:09:54 +0530

    Debugging

--- application/controllers/Pending_purchase_requests.php
+++ application/controllers/Pending_purchase_requests.php
@@ -2,22 +2,25 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Pending_purchase_requests extends CI_Controller {
+  var $session_data;
+
   public function __construct(){
     parent::__construct();
     $this->load->model('Purchase_requests_model');
 
-    $session_data = $this->session->userdata['logged_in'];
+    $this->session_data = $this->session->userdata['logged_in'];
 
-    $permissions = $session_data['permissions'];
-    if(!permissions['view_pending_purchase_reqs']){
+    $permissions = $this->session_data['permissions'];
+    if(!$permissions['A']){
       redirect('dashboard_main');
     }
 
   }
 
   public function index(){
-    $role = $session_data['role'];
-    $dept = $session_data['dept'];
+    $role = $this->session_data['role'];
+    echo $role->role_name;
+    $dept = $this->session_data['dept'];
 
     $data = $this->Purchase_requests_model->getPendingRequests($role,$dept);
 
